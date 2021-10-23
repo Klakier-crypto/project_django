8bce54cdf4a9546adcde2259ede156bfb39974a7
Klakier <jozefnikiel@gmail.com>
2021-10-23 18:30:20 +0200

    secret_key_change

---
+++ klakier/klakier/secret_key.py
@@ -0,0 +1,3 @@
+
+
+SECRET_KEY = 'django-insecure-z9xdadnadr=juzuqy^+hj=go6ta91v)5kw*fo^je+$)i6=#=y3'
--- klakier/klakier/settings.py
+++ klakier/klakier/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from secret_key import SECRET_KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z9xdadnadr=juzuqy^+hj=go6ta91v)5kw*fo^je+$)i6=#=y3'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
